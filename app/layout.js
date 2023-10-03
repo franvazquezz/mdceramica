e3facf408da3901452298f54298df7f6aead6ece
franvazquezz <franvazquez2001@gmail.com>
2023-10-03 16:15:50 -0300

    second commit

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: 'MD Cerámica',
+  description: 'App de gestión de Alumnas',
+  
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+};
