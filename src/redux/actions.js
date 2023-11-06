59dac7b6559f7fd9b2a9e11cdf29f73eaf98c1f0
franvazquezz <franvazquez2001@gmail.com>
2023-11-06 11:48:25 -0300

    deploy 2

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const URL = 'https://mdceramicaback.onrender.com'
-const URL = 'http://localhost:3001'
+const URL = 'https://mdceramicaback.onrender.com:10000'
+// const URL = 'http://localhost:3001'
 
 export const getStudents = () => {
   try {
